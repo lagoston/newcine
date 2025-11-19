daf76323dda21da382cda22475b4d8b89adedeee
lagoston <brunoabrunsfeld@hotmail.com>
2025-11-19 13:15:01 -0300

    Updated stripe-config.ts

--- src/stripe-config.ts
+++ src/stripe-config.ts
@@ -1,7 +1,7 @@
 export const products = {
   premium: {
-    id: 'prod_NEW_ID', prod_TS7VLXMErzpBKQ
-    priceId: 'price_NEW_ID', price_1SVDGXElYXeJYKCBKbqjTvae
+    id: 'prod_TS7VLXMErzpBKQ', // TODO: Replace with new product ID from Stripe
+    priceId: 'price_1SVDGXElYXeJYKCBKbqjTvae', // TODO: Replace with new price ID from Stripe
     name: 'Premium Monthly',
     description: 'CineOracle Premium Monthly Subscription',
     price: 2.99,
