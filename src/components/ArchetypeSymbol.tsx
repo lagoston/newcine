1ad0b27ee1fab894fbe3481d51afb19a4a60b073
lagoston <brunoabrunsfeld@hotmail.com>
2026-05-26 10:55:54 -0300

    Updated ArchetypeSymbol.tsx

--- src/components/ArchetypeSymbol.tsx
+++ src/components/ArchetypeSymbol.tsx
@@ -14,7 +14,7 @@ const SUBCATEGORY_COLORS = {
   B: '#8B5CF6', // Sombrio - Roxo
   K: '#EF4444', // Clássico - Vermelho
   X: '#3B82F6', // Experimental - Azul
-  D: '#FFFFFF', // Denso - Branco
+  D: '#111111', // Denso - Branco
   L: '#10B981', // Leve - Verde
 };
 
