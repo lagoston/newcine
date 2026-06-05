7f2ef67cface3c4bb5b0c72be1409a90097a0b46
lagoston <brunoabrunsfeld@hotmail.com>
2026-06-05 20:55:03 -0300

    Updated banners.ts

--- src/lib/banners.ts
+++ src/lib/banners.ts
@@ -162,7 +162,7 @@ export const banners = {
   id: 'fd',
   name: 'Final Destination Banner',
   isPremium: true,
-  requiredTag: 'death-dodger',
+  requiredTag: 'future-whisperer',
   className: [
     'relative overflow-hidden',
     'bg-[#060606]',
