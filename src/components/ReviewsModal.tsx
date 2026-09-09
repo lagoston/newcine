f05ee375d2c8154c1ea36954e223ad94aeb9981d
lagoston <brunoabrunsfeld@hotmail.com>
2026-09-08 22:03:09 -0300

    Updated ReviewsModal.tsx

--- src/components/ReviewsModal.tsx
+++ src/components/ReviewsModal.tsx
@@ -161,7 +161,7 @@ const ReviewsModal: React.FC<ReviewsModalProps> = ({ movie, onClose, userRating 
     <>
       {createPortal(
         <AnimatePresence>
-          <div className="fixed inset-0 z-[100] flex items-center justify-center p-4">
+          <div className="fixed inset-0 z-[10000] flex items-center justify-center p-4">
             <motion.div
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
